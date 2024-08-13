9c1db30d65c013c009cfc53755803191ae89ba59
David Pontes <pondavid103@gmail.com>
2024-08-12 21:02:08 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="title">tela React</h1>
+      <MyComponent />
+      <hr />
+    
+    </div>
+  );
+}
+
+export default App;
