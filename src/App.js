88e19662a270aa4d4e7e28ac41454901cf6884f8
David Pontes <pondavid103@gmail.com>
2024-08-12 21:05:44 -0300

    add:imagens for container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@
 import React from 'react';
 import './App.css';
 
+// Importar as imagens
+import image1 from './assets/images/image1.jpg';
+import image2 from './assets/images/image2.jpg';
+import image3 from './assets/images/image3.jpeg';
 
 function App() {
+  const MyComponent = () => {
+    return (
+      <div className="image-container">
+        <div className="image-wrapper">
+          <img src={image3} alt="Imagem 3" />
+          <div className="text-overlay">O que você tem certeza do que sabe?</div>
+        </div>
+        <div className="image-wrapper">
+          <img src={image1} alt="Imagem 1" />
+          <div className="text-overlay">Sei que nada Sei</div>
+        </div>
+        <div className="image-wrapper">
+          <img src={image2} alt="Imagem 2" />
+          <div className="text-overlay">Sei Taxar</div>
+        </div>
+        
+      </div>
+    );
+  };
 
   return (
     <div className="App">
-      <h1 className="title">tela React</h1>
+      <h1 className="title">Saber</h1>
       <MyComponent />
       <hr />
     
